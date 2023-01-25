99715defc1b615d2515dd3a59e49a60a0583dc24
voronovski <avoronovski@gmail.com>
2023-01-25 00:54:16 -0800

    main logic implemented

--- Spring Animation/ViewController.swift
+++ Spring Animation/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import SpringAnimation
 
-class ViewController: UIViewController {
-
-    @IBOutlet var demoView: UIView!
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var demoView: SpringView!
     
     @IBOutlet var presetLabel: UILabel!
     @IBOutlet var curveLabel: UILabel!
@@ -17,16 +19,59 @@ class ViewController: UIViewController {
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var delayLabel: UILabel!
     
-    @IBOutlet var runButton: UIButton!
+    @IBOutlet var repeatButton: UIButton!
     
+    //MARK: - Private properties
+    var preset: AnimationPreset!
+    var curve: AnimationCurve!
+    var force: CGFloat!
+    var duration: CGFloat!
+    var delay: CGFloat!
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        demoView.layer.cornerRadius = 10
         
-    }
-
-    @IBAction func runButtonPressed() {
+        demoView.layer.cornerRadius = 10
     }
     
+    // MARK: - IBActions
+    @IBAction func runButtonPressed() {
+        setParameters()
+        setLabels()
+        sendParamsToView()
+        demoView.animate()
+        repeatButton.isHidden = false
+    }
+
+    @IBAction func repeatButtonPressed() {
+        sendParamsToView()
+        demoView.animate()
+    }
+    
+    // MARK: - Private methods
+    private func setParameters() {
+        preset = AnimationPreset.allCases.randomElement() ?? .pop
+        curve = AnimationCurve.allCases.randomElement() ?? .easeIn
+        force = CGFloat.random(in: 1...1.5)
+        duration = CGFloat.random(in: 0.5...2)
+        delay = CGFloat.random(in: 0...1)
+    }
+    
+    private func setLabels() {
+        presetLabel.text = "preset: \(preset.rawValue)"
+        curveLabel.text = "curve: \(curve.rawValue)"
+        forceLabel.text = String(format: "force: %.2f", force)
+        durationLabel.text = String(format: "duration:  %.2f", duration)
+        delayLabel.text = String(format: "delay:  %.2f", delay)
+    }
+    
+    private func sendParamsToView() {
+        demoView.animation = preset.rawValue
+        demoView.curve = curve.rawValue
+        demoView.force = force
+        demoView.duration = duration
+        demoView.delay = delay
+    }
 }
 
